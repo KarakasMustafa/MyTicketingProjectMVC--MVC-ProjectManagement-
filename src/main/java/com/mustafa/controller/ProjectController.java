3f23f0efdb515e70f8902c7eccbf17e940a8e72b
KarakasMustafa <m.isik0229@gmail.com>
2020-11-29 14:46:27 -0600

    added delete method in project controller!

--- src/main/java/com/mustafa/controller/ProjectController.java
+++ src/main/java/com/mustafa/controller/ProjectController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,6 +29,12 @@ public class ProjectController {
         model.addAttribute("managers",userService.findManagers());
 
         return "/project/create";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProject(@PathVariable("id") String id,Model model){
+        projectService.deleteById(id);
+        return "redirect:/project/create";
     }
 
 
