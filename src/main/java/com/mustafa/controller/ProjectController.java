f8b77af9bc815d2f044be2a578553069f4ed6a76
KarakasMustafa <m.isik0229@gmail.com>
2020-11-29 10:49:05 -0600

    added projectDTO class

--- src/main/java/com/mustafa/controller/ProjectController.java
+++ src/main/java/com/mustafa/controller/ProjectController.java
@@ -1,5 +1,22 @@
 package com.mustafa.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
 public class ProjectController {
+
+    @GetMapping("/create")
+    public String createProject(Model model){
+
+        model.addAttribute("project")
+
+        return "/project/create";
+    }
+
 
 }
