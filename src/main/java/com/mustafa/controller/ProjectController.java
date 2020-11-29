9ae278e9de55ff8cef2804609c630c8b7706a328
KarakasMustafa <m.isik0229@gmail.com>
2020-11-29 15:40:25 -0600

    fixed LocalDate issues in insertProject

--- src/main/java/com/mustafa/controller/ProjectController.java
+++ src/main/java/com/mustafa/controller/ProjectController.java
@@ -2,14 +2,20 @@ package com.mustafa.controller;
 
 
 import com.mustafa.dto.ProjectDTO;
+import com.mustafa.dto.UserDTO;
+import com.mustafa.enums.Status;
 import com.mustafa.services.ProjectService;
 import com.mustafa.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/project")
@@ -29,6 +35,15 @@ public class ProjectController {
         model.addAttribute("managers",userService.findManagers());
 
         return "/project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertProject(ProjectDTO project){
+        projectService.save(project);
+        project.setStatus(Status.OPEN);
+
+
+        return "redirect:/project/create";
     }
 
     @GetMapping("/delete/{id}")
--- src/main/java/com/mustafa/dto/ProjectDTO.java
+++ src/main/java/com/mustafa/dto/ProjectDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -18,7 +19,9 @@ public class ProjectDTO {
     private String projectName;
     private String projectCode;
     private UserDTO assignedManager;
+    @DateTimeFormat(pattern = "yyyy-dd-MM")
     private LocalDate startDate;
+    @DateTimeFormat(pattern = "yyyy-dd-MM")
     private LocalDate endDate;
     private String projectDetail;
     private Status status;
