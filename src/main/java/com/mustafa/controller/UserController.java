824425c22100cda9f06a57ddbf5a06ee7f946a33
KarakasMustafa <m.isik0229@gmail.com>
2020-11-28 20:18:15 -0600

    added delete methods in Controller class and html file!

--- src/main/java/com/mustafa/controller/UserController.java
+++ src/main/java/com/mustafa/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,5 +37,13 @@ public class UserController {
         userService.save(user);
 
         return "redirect:/user/create";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") String id){
+
+        userService.deleteById(id);
+        return "redirect:/user/create";
+
     }
 }
